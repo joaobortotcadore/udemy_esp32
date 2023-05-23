316503cef985f718aa5ea5f107c5a1b60aefa303
joaobortotcadore <joaobortotcadore@gmail.com>
2023-05-23 13:43:28 -0300

    chore: complete wifi application
    Refs: Section 8

---
+++ main/tasks_common.h
@@ -0,0 +1,16 @@
+/*
+ * tasks.common.h
+ *
+ *  Created on: 18 de mai. de 2023
+ *      Author: joao.cadore
+ */
+
+#ifndef MAIN_TASKS_COMMON_H_
+#define MAIN_TASKS_COMMON_H_
+
+// WiFi application task
+#define WIFI_APP_TASK_STACK_SIZE		4096
+#define WIFI_APP_TASK_PRIORITY			5
+#define WIFI_APP_TASK_CORE_ID			0
+
+#endif /* MAIN_TASKS_COMMON_H_ */
